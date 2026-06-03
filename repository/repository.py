af8d3ef6ee4d0f88ddd8370e130d4c6e0dc7d4b3
merico341 <lonetfricoto@gmail.com>
2026-06-03 14:41:33 -0300

    update: Database to PostgresSQL and adding a .env file

--- repository/repository.py
+++ repository/repository.py
@@ -1,8 +1,18 @@
+from dotenv import dotenv_values
 from sqlalchemy import create_engine
 from model.base import Base
 
-class Repository():
+env = dotenv_values(".env")
 
+db_adrss = env["DB_ADRSS"]
+db_name = env["DB_NAME"]
+db_user = env["DB_USER"]
+db_passwd = env["DB_PASSWD"]
+db_port = env["DB_PORT"]
+
+class Repository():
+    
     def __init__(self):
-        self.engine = create_engine("sqlite:///banco.db", echo=True)
+        DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_passwd}@{db_adrss}:{db_port}/{db_name}"
+        self.engine = create_engine(DATABASE_URL)
         Base.metadata.create_all(self.engine)
