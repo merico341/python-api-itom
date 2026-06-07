c1f314408f985a27d334647205ace133b307e418
merico341 <lonetfricoto@gmail.com>
2026-06-07 12:09:58 -0300

    update: repository to only instantiate once

--- repository/repository.py
+++ repository/repository.py
@@ -11,8 +11,21 @@ db_passwd = env["DB_PASSWD"]
 db_port = env["DB_PORT"]
 
 class Repository():
-    
-    def __init__(self):
-        DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_passwd}@{db_adrss}:{db_port}/{db_name}"
-        self.engine = create_engine(DATABASE_URL)
-        Base.metadata.create_all(self.engine)
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Repository, cls).__new__(cls)
+            
+            DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_passwd}@{db_adrss}:{db_port}/{db_name}"
+            
+            cls._instance.engine = create_engine(
+                DATABASE_URL,
+                pool_size=10,   
+                max_overflow=20,
+                pool_pre_ping=True
+            )
+            
+            Base.metadata.create_all(cls._instance.engine)
+            
+        return cls._instance
